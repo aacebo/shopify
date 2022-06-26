1d0e3b75766b00fcf05a798e681d99fc04729bea
Alex Acebo <alex.acebo@kustomer.com>
2022-06-26 12:33:51 -0400

    add mui button

--- views/settings/src/App.tsx
+++ views/settings/src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Button, Icon } from '@mui/material';
+import { Add as AddIcon } from '@mui/icons-material';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button
+        style={{ margin: 'auto' }}
+        color='primary'
+        variant='outlined'
+      >
+        <AddIcon color='primary' />
+        Add Store
+      </Button>
     </div>
   );
 }
