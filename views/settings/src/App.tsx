88178f190d207f9b86584dd4fa7b43ec2bec4053
Alex Acebo <alex.acebo@kustomer.com>
2022-06-26 17:18:30 -0400

    Update App.tsx

--- views/settings/src/App.tsx
+++ views/settings/src/App.tsx
@@ -26,25 +26,28 @@ function App() {
         open={newShopDialogOpen}
         onClose={(shop) => {
           setNewShopDialogOpen(false);
-          window.Kustomer.showModal({
-            type: 'redirect',
-            content: {
-              title: 'ADD STORE',
-              iconUrl: `${url.protocol}//${url.host}/assets/icon2.png`,
-              description: 'You will need to go to Shopify to add a store.\nClick Go to Shopify below.',
-              primaryDataKt: 'goToShopify',
-              secondaryDataKt: 'cancelAddStore',
-              showCancelButton: true,
-              actionButton: {
-                text: 'Go to Shopify',
-                linkUrl: `${url.protocol}//${url.host}/auth?shop=${shop}&org=${org}&redirectUri=https://${org}.helpsimply.com/app/channels-and-apps/settings/shopify_sdk`,
-              },
-              alertTagText: 'To connect a new store, make sure you sign out of all other stores in Shopify.',
-              alertTagType: 'warning',
-            }
-          }, () => {
 
-          });
+          if (shop) {
+            window.Kustomer.showModal({
+              type: 'redirect',
+              content: {
+                title: 'ADD STORE',
+                iconUrl: `${url.protocol}//${url.host}/assets/icon2.png`,
+                description: 'You will need to go to Shopify to add a store.\nClick Go to Shopify below.',
+                primaryDataKt: 'goToShopify',
+                secondaryDataKt: 'cancelAddStore',
+                showCancelButton: true,
+                actionButton: {
+                  text: 'Go to Shopify',
+                  linkUrl: `${url.protocol}//${url.host}/auth?shop=${shop}&org=${org}&redirectUri=https://${org}.helpsimply.com/app/channels-and-apps/settings/shopify_sdk`,
+                },
+                alertTagText: 'To connect a new store, make sure you sign out of all other stores in Shopify.',
+                alertTagType: 'warning',
+              }
+            }, () => {
+
+            });
+          }
         }}
       />
     </div>
